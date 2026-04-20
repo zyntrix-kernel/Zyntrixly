8baba67c1fc87d42877ddf58862ca98c8cb75fd1
zyntrix-kernel <howevergamerz@gmail.com>
2026-04-20 23:30:26 +0530

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.firebaseConfig = {
+  apiKey:            "AIzaSyADe7-8VV6flXLwRc3pQtVpallWScxbY90",
+  authDomain:        "zyntrixly.firebaseapp.com",
+  projectId:         "zyntrixly",
+  storageBucket:     "zyntrixly.firebasestorage.app",
+  messagingSenderId: "663603557838",
+  appId:             "1:663603557838:web:634138b6999cf849d5e79f"
+};
